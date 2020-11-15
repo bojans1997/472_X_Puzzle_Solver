d77dcc96188d5370d142a04410dce7ed41df73e8
Zlata-Korotkova <72354931+Zlata-Korotkova@users.noreply.github.com>
2020-11-15 15:09:33 -0500

    Updated AlgorithmA line 4

--- Assignment2/Assignment2/AlgorithmA.py
+++ Assignment2/Assignment2/AlgorithmA.py
@@ -1,7 +1,7 @@
 from queue import PriorityQueue
 import time
 
-timeout = time.time() + 60
+timeout = 60
 
 puzzles = []
 with open('../../Downloads/472_Assignment2-Sunny 2/Assignment2/Assignment2/Resources/testpuzzle.txt') as f:
