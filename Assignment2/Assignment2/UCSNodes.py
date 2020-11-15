a55eb1f483851264154ee32d181a36c29ac0b1b2
sunsetium <patelsunny@live.ca>
2020-11-15 14:44:09 -0500

    deleted unneeded files

--- Assignment2/Assignment2/UCSNodes.py
+++
@@ -1,9 +0,0 @@
-class UCSNodes:
-    """Creation of the Node class for Uniform Cost Search"""
-    def __init__(self,  parent, currentWeight, childrenList):
-        self.parent = parent
-        self.currentWeight = currentWeight
-        self.childrenList = childrenList
-        
-    def getWeight():
-        return currentWeight
